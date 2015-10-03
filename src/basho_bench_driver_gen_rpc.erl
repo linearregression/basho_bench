ead6848cf8c8270d8f7fdd7e17b696fd3076dff2
root <root@myserver.domain.com>
2015-10-03 13:38:48 -0700

    add some test helpers

--- src/basho_bench_driver_gen_rpc.erl
+++ src/basho_bench_driver_gen_rpc.erl
@@ -6,8 +6,10 @@
 %%% http://www.apache.org/licenses/LICENSE-2.0
 %%%
 
-%% * Set ups one or many gen_rpc clients to hit on one or many target gen_rpc server.
-%% * Result collect back to gen_rpc_master for logs
+%% * Brief: 
+%%   Bench test gen_rpc server using one or many gen_rpc clients from multiple erlang nodes.
+%%   Each Erlang nodes can run one or more test processes.
+%% * More Info:
 
 -module(basho_bench_driver_gen_rpc).
 -author("Panagiotis Papadomitsos <pj@ezgr.net>").
@@ -20,16 +22,25 @@
 -include("basho_bench.hrl").
 -define(APP, 'gen_rpc').
 
-%%% Library interface
-%%-export([call/3, call/4, call/5, call/6, cast/3, cast/4, cast/5]).
+%%% Test State
+-record(state, { client,
+                 bucket,
+                 replies }).
 
 %%% ===================================================
 %%% Library interface
 %%% ===================================================
+new(Id) ->
+
+
+
+%%% ===================================================
+%%% Test Helpers
+%%% ===================================================
 ensure_deps_present(App)->
     %% Make sure the path is setup
     case code:which(App) of
-        non_existing -> ?FAIL_MSG("Module=\"~p\" function=ensure_deps_present event=module_not_found module=\"~p\"", [?MODULE, App]);
+        non_existing -> ?FAIL_MSG("function=ensure_deps_present event=module_not_found module=\"~p\"", [App]);
         _ -> {ok, {App, loaded}
     end.
 
@@ -37,12 +48,35 @@ ensure_target_nodes()->
     Nodes   = basho_bench_config:get(gen_rpc_client_nodes),
     Cookie  = basho_bench_config:get(gen_rpc_client_cookie, 'gen_rpc'),
     MyNode  = basho_bench_config:get(gen_rpc_client_mynode, [basho_bench, longnames]),
+    TestTargets = choose_test_target(Hosts, Port, WorkerId)
+    %% Initialize cookie for each of the nodes
+    [begin 
+        N0 = check_target_node(N),
+        true = erlang:set_cookie(N0, Cookie),   
+        {ok, _SlaveApps} = rpc:call(N0, application, ensure_all_started, [?APP]),     
+     end|| N <- Nodes],
+    %% Try to ping each of the nodes
+    ping_each(Nodes).
 
-start_slave() ->
-    %% Starting a slave node with Distributed Erlang
-    {ok, _Slave} = slave:start(?SLAVE_IP, ?SLAVE_NAME, "+K true"),
-    ok = rpc:call(?SLAVE, code, add_pathsz, [code:get_path()]),
-    %% Start the application remotely
-    {ok, _SlaveApps} = rpc:call(?SLAVE, application, ensure_all_started, [gen_rpc]),
-    ok.
+check_target_node(Node) when Node =:= node() ->
+    ?FAIL_MSG("function=check_target_node event=test_driver_and_target_same module=\"~p\"", [Node]).
+    exit(test_target_is_localnode);
+check_target_node(Node) -> Node.
+
+ping_each([]) ->
+    ok;
+ping_each([Node | Rest]) ->
+    case net_adm:ping(Node) of
+        pong ->
+            ping_each(Rest);
+        pang ->
+            ?FAIL_MSG("function=ping_each event=ping_fail node=\"~p\"", [Node])
+    end.
+
+%% Choose a test target to hit to test with different traffic pattern.
+choose_test_target(Hosts, Port, WorkerId)->
+    %% Choose the node using our ID as a modulus
+    TargetHost = lists:nth((WorkerId rem length(Hosts)+1), Hosts),
+    ?INFO("function=choose_test_target s:~p for worker ~p\n", [TargetHost, Port, WorkerId]).
+    TargetHost.
 
