899e789b313444e26a0cf2cf47a0d10d3e13a53a
root <root@myserver.domain.com>
2015-10-05 12:29:17 -0700

    fix startup script and point to presjim repo

--- example_script/run_target.sh
+++ example_script/run_target.sh
@@ -2,5 +2,17 @@
 echo "Start a target server"
 TARGET_NODE_NAME=gen_rpc_target@127.0.0.1
 TEST_SYS_COOKIE=genrpc
-MY_APP=gen_rpc_app
-erl -name ${TARGET_NODE_NAME} -setcookie ${TEST_SYS_COOKIE} -pa ./deps/*/ebin -pa ./deps/gen_rpc/_build/dev/lib/*/ebin -s ${MY_APP}
+
+#erl -name ${TARGET_NODE_NAME} -setcookie ${TEST_SYS_COOKIE} -pa ./deps/*/ebin -pa ./deps/gen_rpc/_build/dev/lib/*/ebin --eval 'application:start(gen_rpc)'
+# Start Beehive
+echo "Starting gen_rpc Bench Test"
+eval "erl \
+    -name \"${TARGET_NODE_NAME}\" \
+    -setcookie \"${TEST_SYS_COOKIE}\" \
+    -pa deps/*/ebin -pa .gen_rpc/_build/dev/lib/*/ebin  \
+    -eval \"application:start(crypto)\" \
+    -eval \"application:start(asn1)\" \
+    -eval \"application:start(public_key)\" \
+    -eval \"application:start(ssl)\" \
+    -eval \"application:start(lager)\" 
+    -eval \"application:start(gen_rpc)\""
