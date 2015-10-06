6f92c925760f824c6c9c53a2921a69ce97a1b0f5
root <root@myserver.domain.com>
2015-10-05 20:45:05 -0700

    make node and cookie as var

--- example_script/run_test.sh
+++ example_script/run_test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo on
+echo "Start a target server"
+TARGET_NODE_NAME=gen_rpc_benchmark@127.0.0.1
+TEST_SYS_COOKIE=genrpc
 rm -rf ./tests/*
-./basho_bench -N gen_rpc_benchmark@127.0.0.1 -C gen_rpc ./examples/gen_rpc.config
+./basho_bench -N ${TARGET_NODE_NAME} -C ${TEST_SYS_COOKIE} ./examples/gen_rpc.config
